9671cf0067fc9a757eb4082ce0cfc2f20a5fd9d6
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-04-16 17:50:21 +0300

    Create the login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import Nav from "../components/Nav";
+import { FaTruck } from "react-icons/fa";
+import Footer from "../components/Footer";
+import Swal from "sweetalert2";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+
+  function login(e) {
+    e.preventDefault();
+    console.log(formData);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ formData }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.errors);
+        if (!data.errors) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your login was successful",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        } else {
+          setErrors(data.errors);
+        }
+      });
+  }
+  return (
+    <div>
+      <Nav />
+      <section className="bg-slate-300">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow-lg shadow-slate-900 md:mt-0 sm:max-w-md xl:p-0">
+            <div className="p-6 space-y-3 sm:p-8">
+              <div className="flex justify-between">
+                <FaTruck className="text-indigo-500 text-3xl" />
+                <h1 className="text-xl font-bold font-serif">Sign Up</h1>
+              </div>
+              <form onSubmit={login} className="space-y-3">
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    placeholder="emmanuel_ke"
+                    required
+                  />
+                  {errors.username && (
+                    <p className="text-xs text-red-600">{errors.username}!</p>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    value={formData.pasword}
+                    onChange={handleInputChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    required
+                  />
+                  {errors.password && (
+                    <p className="text-xs text-red-600">{errors.password}!</p>
+                  )}
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full bg-indigo-500 font-bold font-serif text-gray-800 p-2 rounded-sm"
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -26,7 +26,6 @@ function SignUp() {
 
   function register(e) {
     e.preventDefault();
-    console.log(formData);
     fetch("/users", {
       method: "POST",
       headers: {
@@ -76,7 +75,7 @@ function SignUp() {
                     onChange={handleInputChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="Emmanuel Matthews"
-                    // required
+                    required
                   />
                   {errors.full_name && (
                     <span className="text-xs text-red-600">
@@ -99,7 +98,7 @@ function SignUp() {
                     onChange={handleInputChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="emmanuel@gmail.com"
-                    // required
+                    required
                   />
                   {errors.email &&
                     errors.email.map((error, index) => {
@@ -121,7 +120,7 @@ function SignUp() {
                     onChange={handleInputChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="emmanuel_ke"
-                    // required
+                    required
                   />
                   {errors.username &&
                     errors.username.map((error, index) => {
@@ -143,7 +142,7 @@ function SignUp() {
                     value={formData.pasword}
                     onChange={handleInputChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    // required
+                    required
                   />
                   {errors.password &&
                     errors.password.map((error, index) => {
@@ -165,7 +164,7 @@ function SignUp() {
                     value={formData.password_confirmation}
                     onChange={handleInputChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    // required
+                    required
                   />
                   {errors.password_confirmation && (
                     <span className="text-xs text-red-600">
