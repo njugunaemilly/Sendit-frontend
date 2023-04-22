0a0d42e289ede8dd91d0977be0280727dfb0399c
+ <khalifnashon@gmail.com>
2023-04-22 07:40:50 +0300

    custom button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,8 @@ import { BiUserCircle } from "react-icons/bi";
 
 const Nav = () => {
   const [state, setState] = useState(false);
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   // Replace javascript:void(0) paths with your paths
   const navigation = [
@@ -19,6 +21,7 @@ const Nav = () => {
     document.onclick = (e) => {
       const target = e.target;
       if (!target.closest(".menu-btn")) setState(false);
+      if (!target.closest(".profile-btn")) setNav(false);
     };
   }, []);
 
@@ -120,11 +123,11 @@ const Nav = () => {
             </a>
           </div>
           {/* Dropdown */}
-          <div class="relative inline-block m-2 text-left">
-            <div>
+          <div className="relative inline-block m-2 text-left">
+            <div onClick={handleClick}>
               <button
                 type="button"
-                class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-slate-900 hover:text-white"
+                className="profile-btn inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-slate-900 hover:text-white"
                 id="menu-button"
                 aria-expanded="true"
                 aria-haspopup="true"
@@ -132,7 +135,7 @@ const Nav = () => {
                 <BiUserCircle size={20} /> 
                 <h1>User</h1>
                 <svg
-                  class="-mr-1 h-5 w-5 text-gray-400"
+                  className="-mr-1 h-5 w-5 text-gray-400"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -147,17 +150,22 @@ const Nav = () => {
             </div>
 
             <div
-              class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            
+              className= {
+                !nav
+                ? "hidden"
+                :"absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              }
               role="menu"
               aria-orientation="vertical"
               aria-labelledby="menu-button"
               tabindex="-1"
             >
-              <div class="py-1" role="none">
+              <div className="py-1" role="none">
                 {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
                 <a
                   href="#"
-                  class="text-gray-700 block px-4 py-2 text-sm"
+                  className="text-gray-700 block px-4 py-2 text-sm"
                   role="menuitem"
                   tabindex="-1"
                   id="menu-item-0"
@@ -166,7 +174,7 @@ const Nav = () => {
                 </a>
                 <a
                   href="#"
-                  class="text-gray-700 block px-4 py-2 text-sm"
+                  className="text-gray-700 block px-4 py-2 text-sm"
                   role="menuitem"
                   tabindex="-1"
                   id="menu-item-1"
@@ -175,7 +183,7 @@ const Nav = () => {
                 </a>
                 <a
                   href="#"
-                  class="text-gray-700 block px-4 py-2 text-sm"
+                  className="text-gray-700 block px-4 py-2 text-sm"
                   role="menuitem"
                   tabindex="-1"
                   id="menu-item-2"
@@ -184,11 +192,10 @@ const Nav = () => {
                 </a>
                 <button
                     type="submit"
-                    class="text-gray-700 block w-full px-4 py-2 text-left text-sm"
+                    className="text-gray-700 block w-full px-4 py-2 text-left text-sm border-none"
                     role="menuitem"
                     tabindex="-1"
                     id="menu-item-3"
-                    className="border-none"
                   >
                     Sign out
                 </button>
