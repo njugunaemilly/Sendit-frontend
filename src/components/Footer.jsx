b213d49bdbe9153c2e4355427080efa3ca91bbce
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-04-27 12:22:37 +0300

    Remove the check button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="pb-6">
-              <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+              <Link href={item.href} className="text-sm leading-6 text-white hover:text-gray-400">
                 {item.name}
               </Link>
             </div>
--- src/components/SingleOrderAdmin.jsx
+++ src/components/SingleOrderAdmin.jsx
@@ -167,23 +167,6 @@ export default function SingleOrderAdmin({ id }) {
                 Ksh: {filteredParcel[0].price}
               </dd>
             </div>
-            <Switch
-              checked={enabled}
-              onChange={setEnabled}
-              className={classNames(
-                enabled ? "bg-indigo-600" : "bg-gray-200",
-                "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              )}
-            >
-              <span className="sr-only">Use setting</span>
-              <span
-                aria-hidden="true"
-                className={classNames(
-                  enabled ? "translate-x-5" : "translate-x-0",
-                  "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
-                )}
-              />
-            </Switch>
             <div className="sm:col-span-1">
               <dt className="text-sm font-medium text-gray-500">
                 Email address
