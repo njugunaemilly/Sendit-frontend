b1456c0e2fa2f50b65c3d546d1112584f9575f59
Emilly <emilly.wanjiku@student.moringaschool.com>
2023-04-30 19:12:23 +0300

    Update currency and courntries order form

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -226,27 +226,6 @@ export default function OrderForm() {
                       )}
                     </div>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="country"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Country/Region
-                      </label>
-                      <select
-                        id="country"
-                        name="country"
-                        autoComplete="country-name"
-                        value={formData.country}
-                        onChange={handleInputChange}
-                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      >
-                        <option>Kenya</option>
-                        <option>Tanzania</option>
-                        <option>Uganda</option>
-                      </select>
-                    </div>
-
                     <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                       <label
                         htmlFor="weight"
@@ -270,7 +249,7 @@ export default function OrderForm() {
 
                       {formData.price && (
                         <span className="text-xl text-green-600">
-                          {`Price: ${formData.price}`}!
+                          {`Price: Ksh.${formData.price}`}!
                         </span>
                       )}
                     </div>
